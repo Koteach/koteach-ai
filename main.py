90af65264507ac815ca06055959b01fbc01941a0
Peter Bulloch <peterbllch2@gmail.com>
2023-11-25 21:12:09 +0900

    Add base structure

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from openai import OpenAI
+from train import *
 
 
 def main():
-    client = OpenAI()
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-        ]
-    )
-
-    print(completion.choices[0].message)
+    construct_index(".")
 
 if __name__ == "__main__":
     main()
